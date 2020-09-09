d696e29448f730e8d44f5b438d79a17ae3210231
ShaigroRB <robin.boucher@epita.fr>
2020-09-10 00:52:35 +0200

    most generation functions are now methods for Weapon & WeaponPart + add WeaponPattern
    - Add WeaponPattern to avoid having lots of arguments in each method
    - Add generateSvgGivenPatterns to Weapon
    - isPatternAvailable returns true if the patterns list is empty, otherwise it searchs in the list if the pattern is there. An empty list means that it has every pattern. A list with a negative index means that the part has no pattern (like the cross for the healgun).

--- js/viewers/classes/weapon.js
+++ js/viewers/classes/weapon.js
@@ -2,7 +2,7 @@ class WeaponPart {
     /**
      * 
      * @param {string} name 
-     * @param {number[]} patterns 
+     * @param {number[]} patterns if the list is empty, it means that all patterns are available
      */
     constructor(name, patterns) {
         this.name = name;
@@ -11,36 +11,122 @@ class WeaponPart {
 
     isPatternAvailable = (pattern) => {
         const nbPattern = +pattern;
-        return this.patterns.includes(nbPattern);
+        return this.patterns.length === 0
+            || this.patterns.includes(nbPattern);
+    }
+
+    generateBaseForSvg = (folder) => {
+        return '<image width="100%" height="100%"'
+            + 'xlink:href="imgs/skins/' + folder + '/' + this.name + '_base.png" />';
+    };
+
+    generatePatternWithFilterForSvg = (
+        folder,
+        pattern, filter_name
+    ) => {
+        return '<image width="100%" height="100%"'
+            + 'xlink:href="imgs/skins/' + folder + '/' + this.name + '_' + pattern + '.png"'
+            + 'filter="url(#' + filter_name + ')" />';
+    };
+}
+
+class WeaponPattern {
+    /**
+     * 
+     * @param {string} name name of the pattern
+     * @param {number} index index of the pattern
+     * @param {string} color hex color
+     */
+    constructor(name, index, color) {
+        this.name = name;
+        this.index = index;
+        this.color = color;
     }
 }
 
 class Weapon {
     /**
      * 
-     * @param {string} basename 
+     * @param {string} folder 
      * @param {WeaponPart[]} parts 
      */
-    constructor(basename, parts) {
-        this.basename = basename;
+    constructor(folder, parts) {
+        this.folder = folder;
         this.parts = parts;
     }
 
-    generateBaseAsSvg = () => {
-        let resultBase = generateWeaponPartBaseForSvg(this.basename, this.basename);
-        this.parts.forEach((part) => {
-            resultBase += generateWeaponPartBaseForSvg(this.basename, part.name);
-        });
-        return resultBase;
-    }
+    _generateFilterForSvg = (name, color) => {
+        const filter_name = this.folder + '_mask_' + name;
+        return {
+            "filter_name": filter_name,
+            "filter_svg":
+                '<filter id="' + filter_name + '">'
+                + '<feFlood flood-color="' + color + '" result="flood" />'
+                + '<feComposite in="SourceGraphic" in2="flood" operator="arithmetic" k1="1" k2="0" k3="0" k4="0" />'
+                + '</filter>'
+        };
+    };
 
-    generatePatternAsSvgWithFilter = (pattern, filter_name) => {
-        let resultPattern = generateWeaponPartPatternWithFilterForSvg(this.basename, this.basename, pattern, filter_name);
-        this.parts.forEach((part) => {
-            if (part.isPatternAvailable(pattern)) {
-                resultPattern += generateWeaponPartPatternWithFilterForSvg(this.basename, part.name, pattern, filter_name);
+    /**
+     * @param {WeaponPattern[]} weaponPatterns
+     */
+    _generateDefsForSvg = (weaponPatterns) => {
+        const defsAndFilters = {
+            "defs": "",
+            "filters": []
+        };
+        defsAndFilters.defs += '<defs id="' + this.folder + '_filters_defs">\n';
+        weaponPatterns.forEach((pattern) => {
+            if (pattern.index > 0) {
+                const filter = this._generateFilterForSvg(pattern.name, pattern.color);
+                defsAndFilters.defs += filter.filter_svg + '\n';
+                defsAndFilters.filters.push(filter.filter_name);
             }
         });
-        return resultPattern;
+        defsAndFilters.defs += '</defs>\n';
+        return defsAndFilters;
+    }
+
+    /**
+     * @param {string[]} filters
+     * @param {WeaponPattern[]} weaponPatterns
+     */
+    _generateGroupeForSvg = (filters, weaponPatterns) => {
+        let group = '<g id="' + this.folder + '_parts_and_patterns_group">';
+        this.parts.forEach((part) => {
+            let imgPart = part.generateBaseForSvg(this.folder);
+            let filterIndex = 0;
+            weaponPatterns.forEach((pattern) => {
+                if (pattern.index > 0) {
+                    if (part.isPatternAvailable(pattern.index)) {
+                        imgPart += part.generatePatternWithFilterForSvg(this.folder, pattern.index, filters[filterIndex]);
+                    }
+                    filterIndex++;
+                }
+            });
+            group += imgPart;
+        });
+        group += '</g>\n';
+        return group;
+    }
+
+    /**
+     * 
+     * @param {number} width width of the svg
+     * @param {number} height height of the svg
+     * @param {WeaponPattern[]} weaponPatterns patterns applied to the weapon svg
+     */
+    generateSvgGivenPatterns = (
+        width, height,
+        weaponPatterns
+    ) => {
+        let svgHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" '
+            + 'width="' + width + '" height="' + height + '">';
+        const defsAndFilters = this._generateDefsForSvg(weaponPatterns);
+        const group = this._generateGroupeForSvg(defsAndFilters.filters, weaponPatterns);
+        svgHTML += defsAndFilters.defs;
+        svgHTML += group;
+        svgHTML += '</svg>';
+        return svgHTML;
     }
 };
--- js/viewers/skin.js
+++ js/viewers/skin.js
@@ -1,55 +1,7 @@
 const weapons = [
-    new Weapon("burst_rifle", [new WeaponPart("burst_rifle_reload", [11, 12])])
 ];
 
-const generateFilterForSvg = (name, color) => {
-    const filter_name = 'mask_' + name;
-    return {
-        "filter_name": filter_name,
-        "filter_svg":
-            '<filter id="' + filter_name + '">'
-            + '<feFlood flood-color="' + color + '" result="flood" />'
-            + '<feComposite in="SourceGraphic" in2="flood" operator="arithmetic" k1="1" k2="0" k3="0" k4="0" />'
-            + '</filter>'
-    };
-};
-
-const generateWeaponPartBaseForSvg = (weapon, part) => {
-    return '<image width="100%" height="100%"'
-        + 'xlink:href="imgs/skins/' + weapon + '/' + part + '_base.png" />';
-};
-
-const generateWeaponPartPatternWithFilterForSvg = (
-    weapon, part,
-    pattern, filter_name
-) => {
-    return '<image width="100%" height="100%"'
-        + 'xlink:href="imgs/skins/' + weapon + '/' + part + '_' + pattern + '.png"'
-        + 'filter="url(#' + filter_name + ')" />';
-};
-
-const defs = document.getElementById("objs_definition_svg");
-const imgsGroup = document.getElementById("group_imgs_svg");
-
-/**
- * 
- * @param {Weapon} weapon 
- * @param {string} pattern_name 
- * @param {number} pattern 
- * @param {string} color 
- */
-const generatePatternAndItsFilterForSvg = (
-    weapon,
-    pattern_name, pattern, color
-) => {
-    if (pattern > 0) {
-        const filterSvg = generateFilterForSvg(pattern_name, color);
-        const patternSvg = weapon.generatePatternAsSvgWithFilter(pattern, filterSvg.filter_name);
-
-        defs.innerHTML += filterSvg.filter_svg;
-        imgsGroup.innerHTML += patternSvg;
-    }
-};
+const skinViewer = document.getElementById("skin-viewer");
 
 /**
  * 
@@ -65,16 +17,12 @@ const generateSvgForSkin = (
     pattern1, pattern1_color,
     pattern2, pattern2_color
 ) => {
-    //clear the svg
-    defs.innerHTML = "";
-    imgsGroup.innerHTML = "";
+    const weaponPatterns = [
+        new WeaponPattern("base", base, base_color),
+        new WeaponPattern("pattern1", pattern1, pattern1_color),
+        new WeaponPattern("pattern2", pattern2, pattern2_color)
+    ];
 
-    const weapon = weapons[0];
-    const weaponBase = weapon.generateBaseAsSvg();
-
-    imgsGroup.innerHTML += weaponBase;
-
-    generatePatternAndItsFilterForSvg(weapon, "base", base, base_color);
-    generatePatternAndItsFilterForSvg(weapon, "pattern1", pattern1, pattern1_color);
-    generatePatternAndItsFilterForSvg(weapon, "pattern2", pattern2, pattern2_color);
+    const weapon = weapons[randomGivenMinAndMax(0, weapons.length)];
+    skinViewer.innerHTML = weapon.generateSvgGivenPatterns(250, 144, weaponPatterns);
 };
